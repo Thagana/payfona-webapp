446da5e48385ac92efa1f6a7ed9cf0f129a921bf
Samuel Mothwa <samuel@flatrocksolutions.co.za>
2023-03-21 13:07:38 +0200

    correct web endpoint

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,5 +1,5 @@
 export default {
-  // SERVER_URL: "https://payfona-api-staging.up.railway.app",
-  SERVER_URL: "http://localhost:4000",
+  SERVER_URL: "https://payfona-api-staging.up.railway.app",
+  // SERVER_URL: "http://localhost:4000",
   PAY_STACK_PUBLIC: "pk_test_9747c31bc1d86843545348f5984a58769061a875"
 }
